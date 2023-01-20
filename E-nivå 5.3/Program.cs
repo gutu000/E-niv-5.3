e223e13333e94faf352b71b6e2ac910df85fea71
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:49:03 +0100

    Add project files.

---
+++ E-nivå 5.3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] h = new int[3];
+
+Console.WriteLine("Skriv in 3 tal");
+
+for (int i = 0; i < 3; i++)
+{
+    Console.Write("Tal: ");
+    h[i] = int.Parse(Console.ReadLine());
+}
+int sum = h[0] + h[1] + h[2];
+{
+    Console.WriteLine("Summan av dina tal är " + sum);
+}
+
+
+
+
+
+
